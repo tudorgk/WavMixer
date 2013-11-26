e56c1d541164ccf707b6549729111d694a011a62
tudorgk <tudorgk@gmail.com>
2013-11-26 18:05:18 +0200

    First commit

---
+++ TestUploader/Form1.Designer.cs
@@ -0,0 +1,122 @@
+namespace TestUploader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.uploadButton = new System.Windows.Forms.Button();
+            this.txtFileName1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtFileName2 = new System.Windows.Forms.TextBox();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(371, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.Location = new System.Drawing.Point(371, 129);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(75, 23);
+            this.uploadButton.TabIndex = 1;
+            this.uploadButton.Text = "Upload";
+            this.uploadButton.UseVisualStyleBackColor = true;
+            this.uploadButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtFileName1
+            // 
+            this.txtFileName1.Location = new System.Drawing.Point(12, 31);
+            this.txtFileName1.Name = "txtFileName1";
+            this.txtFileName1.Size = new System.Drawing.Size(353, 20);
+            this.txtFileName1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(371, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 20);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Browse 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtFileName2
+            // 
+            this.txtFileName2.Location = new System.Drawing.Point(12, 72);
+            this.txtFileName2.Name = "txtFileName2";
+            this.txtFileName2.Size = new System.Drawing.Size(353, 20);
+            this.txtFileName2.TabIndex = 4;
+            this.txtFileName2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 445);
+            this.Controls.Add(this.txtFileName2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtFileName1);
+            this.Controls.Add(this.uploadButton);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Wav Mixer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button uploadButton;
+        private System.Windows.Forms.TextBox txtFileName1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtFileName2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
